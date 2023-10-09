3251567430f317b5c6d68efc283c693ae9d25eb0
migiamp <147429494+migiamp@users.noreply.github.com>
2023-10-09 18:54:35 +0200

    hw1
    hw1-statistics

---
+++ hw1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(119, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 260);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(666, 340);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(429, 255);
+            this.panel3.TabIndex = 1;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(119, 340);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(389, 255);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(666, 39);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(429, 260);
+            this.panel4.TabIndex = 3;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 637);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ hw1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //Draw a line
+            Graphics g1 = e.Graphics;
+            Pen p = new Pen(Color.Black, 5);
+            Point x1 = new Point(10, 10);
+            Point x2 = new Point(100, 75);
+            e.Graphics.DrawLine(p, x1, x2);
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e2)
+        {
+            //Draw a rectangle
+            Graphics g2 = e2.Graphics;
+            Pen rectangle = new Pen(Color.Green, 5);
+            e2.Graphics.DrawRectangle(rectangle, 10, 10, 100, 75);
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e3)
+        {
+            Graphics g2 = e3.Graphics;
+            Pen circle = new Pen(Color.BlueViolet, 5);
+            e3.Graphics.DrawEllipse(circle, 10, 20, 90, 90);
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e4)
+        {
+            Graphics g2 = e4.Graphics;
+            Pen circle = new Pen(Color.Red, 15);
+            e4.Graphics.DrawEllipse(circle, 10, 20, 15, 15);
+        }
+    }
+}
